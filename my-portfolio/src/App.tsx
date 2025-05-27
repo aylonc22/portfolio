f6cdc9784ce191df9bd4c33ce5e1ba3df481bbe5
Aylon <aylonc1@gmail.com>
2025-05-27 17:12:05 +0300

    added expirence section

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -65,9 +65,39 @@ function App() {
         </div>     
     </header>
 
-    <section>
-      <h2>Expirence</h2>
-    </section>
+  <section>
+  <h2>Experience</h2>
+  <div className="experience-list">
+    <div className="experience-item">
+      <h3>Tombot X <span>Oct 2024 – Present</span></h3>
+      <p className="role">Software Developer · Remote</p>
+      <p className="desc">
+        Developed backend REST APIs and frontend React components for real-time social listening and automation.
+         Led the design and deployment of a multi-service Dockerized architecture on AWS ECS. Built CI/CD pipelines and monitoring for platform stability, 
+         and integrated AI modules (e.g., ChatGPT) to automate engagement workflows.
+      </p>
+    </div>
+    <div className="experience-item">
+      <h3>Freelance <span>2023 – Present</span></h3>
+      <p className="role">Full Stack Developer · Israel</p>
+      <p className="desc">
+        Delivered fullstack React and Node.js applications from concept to deployment.
+         Implemented secure JWT-based authentication, deployment workflows, and worked closely with clients like N.B. Solutions
+          and Aviel Marketing to meet deadlines and feature requirements.
+      </p>
+    </div>
+    <div className="experience-item">
+      <h3>IAF, Palmahim <span>2019 – 2022</span></h3>
+      <p className="role">Software Developer · Military Service</p>
+      <p className="desc">
+        Engineered UAV communication systems using C# and Node.js. Led modernization efforts 
+        to replace outdated telemetry decoders with optimized solutions, coordinated deployment cycles, and received 4 Certificates of 
+        Excellence for innovation and operational impact.
+      </p>
+    </div>
+  </div>
+</section>
+
 
   <section>
     <h2>Projects</h2>
